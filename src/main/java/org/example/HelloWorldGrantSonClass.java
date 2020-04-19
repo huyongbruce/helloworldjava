9bf98331a9dff99d5b25c16029d3cf459a2a2cd9
huyong <huyong@yuewen.com>
2020-04-19 18:03:51 +0800

    hotfix 0419

--- src/main/java/org/example/HelloWorldGrantSonClass.java
+++ src/main/java/org/example/HelloWorldGrantSonClass.java
@@ -4,6 +4,7 @@ public class HelloWorldGrantSonClass extends HelloWorldDadClass {
 
     public String GrantSonMessage()
     {
+        //fix hotfix0419
         return "GrantSon Message!!";  
     }
 }
