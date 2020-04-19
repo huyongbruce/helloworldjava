e7aeb20fcf2e74e20198cf95996625a631daacb7
huyong <huyong@yuewen.com>
2020-04-19 19:41:31 +0800

    cherry-pick A

--- src/main/java/org/example/HelloWorldGrantSonClass.java
+++ src/main/java/org/example/HelloWorldGrantSonClass.java
@@ -5,6 +5,8 @@ public class HelloWorldGrantSonClass extends HelloWorldDadClass {
     public String GrantSonMessage()
     {
         //fix hotfix0419
+        //cherry-pickA
+
         return "GrantSon Message!!";  
     }
 }
